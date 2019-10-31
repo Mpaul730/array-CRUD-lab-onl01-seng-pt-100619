e6b543bf9ee2919eba2ed76959ab178eaae8515b
Mark Paul <mpaul730@gmail.com>
2019-10-31 18:10:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,6 @@ def add_element_to_end_of_array(array, element)
   goats = ["Goku", "Luffy", "Guts", "Itachi"]
   goats.push("Afro Samurai")
   puts goats.inspect
-  end
 end
 
 def add_element_to_start_of_array(array, element)
