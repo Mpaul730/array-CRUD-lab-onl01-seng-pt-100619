6040a3691d3e1214074e080e04b4ce2190129ea9
Mark Paul <mpaul730@gmail.com>
2019-10-31 17:43:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
 goats = ["Goku", "Luffy", "Guts", "Itachi"]
 goats << "Afro"
-puts goats.inspect
+puts goats
 end
 
 def add_element_to_start_of_array(array, element)
