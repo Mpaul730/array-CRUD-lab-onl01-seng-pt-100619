53d25c544b9693757b2cd82dfbbe9bccf5495ac7
Mark Paul <mpaul730@gmail.com>
2019-10-31 18:14:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,8 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning"]
-  goats.push("arrays!")
-  puts goats.inspect
+  array.push("arrays!")
+  puts array.inspect
 end
 
 def add_element_to_start_of_array(array, element)
